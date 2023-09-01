159b75dc3545e932a6ce38a165c7d9bde16f60b6
reAchi301 <142269072+reAchi301@users.noreply.github.com>
2023-08-31 21:54:11 -0600

    Update emeka.py
    Modificación de formato, adición de funciones y una mejor estrucutración

--- emeka.py
+++ emeka.py
@@ -1,81 +1,109 @@
-#Darle una calida bienvenida al usuario
+print("Bienvenido a EmeK\nTu calculadora de valores de mercado fubtolístico de"
+      "confianza\nInstrucciones generales:\nResponde todo tal como se te pide, para"
+      "respuestas tipo cadena escribir todo en minúsculas")
 
-print("Bienvenido a EmeK\nTu calculadora de valores de mercado fubtolístico de confianza\nInstrucciones generales:\nResponde todo tal como se te pide, para respuestas tipo cadena escribir"
-      " todo en minúsculas")
-#definir una variable de cadena con entrada para reconocer el nombre del jugador
-nombre_jugador=(str(input("Introduce el nombre de un jugador de las 5 grandes ligas de Europa:\n(Premier League,La Liga, Bundesliga, Serie A, Ligue 1)\n")))
-#definir una variable entera para poder ir acumulando una suma de resultados para que al final el programa pueda desplegar un resultado en base a la suma
+#Variable de entrada para guardar el nombre del jugador
+nombre_jugador=(str(input("Introduce el nombre de un jugador de las 5 grandes"
+     "ligas de Europa:\n(Premier League, La Liga,"
+     "Bundesliga, Serie A, Ligue 1)\n")))
+
 resultado=0
-#definir variable de entrada, valor de cadena para guardar la liga del jugador, en base a que liga es la suma que se le va a dar con condicionales se les dara un valor distinto a la varia
-#ble resultado
-liga_jugador=(str(input("Introduce el nombre de la liga en la que juega el jugador(Premier League,La Liga, Bundesliga, Serie A, Ligue 1)\n")))
+#Funciones que van a cumplir con el objetivo de sumar nuevos valores a la variable
+#resultado y así empezar a determinar el rango en el que se encuentra el jugador
+def suma_maxima(resultado):
+    res_5=resultado+5
+    return res_5
+def suma_destacable(resultado):
+    res_4=resultado+4
+    return res_4
+def suma_intermedia(resultado):
+    res_3=resultado+3
+    return res_3
+def suma_pequeña(resultado):
+    res_2=resultado+2
+    return res_2
+def suma_unitaria(resultado):
+    res_1=resultado+1
+    return res_1
+def suma_nula(resultado):
+    res_cero=resultado+0
+    return res_cero
+
+#Variable de entrada para guardar la liga del jugador
+liga_jugador=(str(input("Introduce el nombre de la liga en la que juega el"
+     "jugador(Premier League,La Liga, Bundesliga, Serie A, Ligue 1)\n")))
 
 if liga_jugador == "premier league":
-    resultado=resultado+5
+    resultado = suma_maxima(resultado)
 elif liga_jugador == "la liga":
-    resultado=resultado+4
+    resultado = suma_destacable(resultado)
 elif liga_jugador == "bundesliga":
-    resultado=resultado+3
+    resultado= suma_intermedia(resultado)
 elif liga_jugador == "serie a":
-    resultado=resultado+2
+    resultado= suma_pequeña(resultado)
 elif liga_jugador == "ligue 1":
-    resultado=resultado+1
-#definir otra variable de entrada, esta vez entera para guardar la edad del jugador, posteriormente mediante el uso de condicionales en base a los rangos de edad del jugador cambiar nueva
-#mente el valor de la variable resultado
+    resultado= suma_unitaria(resultado)
+#Variable de entrada para guardar la edad del jugador
 edad_jugador=(int(input("Introduzca la edad del jugador\n")))
 
 if  edad_jugador >=17 and edad_jugador <=23 :
-    resultado=resultado+5
+    resultado = suma_maxima(resultado)
 elif edad_jugador  >=24 and edad_jugador <=27:
-    resultado=resultado+3
+    resultado= suma_intermedia(resultado)
 elif edad_jugador  >=28 and edad_jugador <=34:
-    resultado=resultado+2
+    resultado= suma_pequeña(resultado)
 elif edad_jugador  >=35 and edad_jugador <=41:
-    resultado=resultado+1
+    resultado=suma_unitaria(resultado)
 elif edad_jugador >=42:
-    resultado=resultado+0
+    resultado=suma_nula(resultado)
 
-#Darle nuevas instrucciones al usuario
-#variable de entrada de tipo entero para poder denominar la nacionalidad del jugador y añadirle un nuevo valor a la variable resultado
-print("Escribe 1 para nacionalidades TOP 5:francia","brasil","inglaterra","argentina","bélgica\n")
-print("Escribe 2 para nacionalidades TOP10: croacia","paises bajos","italia","portugal","españa\n")
-print("Escribe 3 para nacionalidades TOP 15: estados unidos","méxico","suiza","marruecos","alemania\n")
+print("Escribe 1 para nacionalidades TOP 5:francia","brasil","inglaterra",
+      "argentina","bélgica\n")
+print("Escribe 2 para nacionalidades TOP10: croacia","paises bajos",
+      "italia","portugal","españa\n")
+print("Escribe 3 para nacionalidades TOP 15: estados unidos","méxico",
+      "suiza","marruecos","alemania\n")
 
+#Variable de entrada para guardar la nacion del jugador
 nacion_jugador=(int(input("Introduce el numero de la nacionalidad del jugador\n")))
 if nacion_jugador==1:
-    resultado=resultado+5
+    resultado=suma_maxima(resultado)
 elif nacion_jugador==2:
-    resultado=resultado+3
+    resultado=suma_intermedia(resultado)
 elif nacion_jugador==3:
-    resultado=resultado+1
+    resultado=suma_unitaria(resultado)
 else:
-    resultado=resultado+0
+    resultado=suma_nula(resultado)
 
-#definir variable de entrada  de cadena para guardar la posicion del jugador, en base a que posición sea va a sere la suma que se le va a dar mediante
-#condicionales se les dara un valor distinto a la variable resultado
-posicion_jugador=(str(input("Introduce la posición del jugador: portero, defensor, mediocampista, delantero)\n")))
+#Variable de entrada para guardar la posición del jugador
+posicion_jugador=(str(input("Introduce la posición del jugador: portero,"
+     "defensor, mediocampista, delantero\n")))
 
 if posicion_jugador == "delantero":
-    resultado=resultado+6
+    resultado=suma_maxima(resultado)
 elif posicion_jugador == "mediocampista":
-    resultado=resultado+5
+    resultado=suma_destacable(resultado)
 elif posicion_jugador == "defensor":
-    resultado=resultado+4
+    resultado=suma_intermedia(resultado)
 elif posicion_jugador == "portero":
-    resultado=resultado+1
-#funcion para dar el resultado final de todas las sumas, el cual se va a determinar por (resultado de sumas/numero de sumas) en este caso son 4 sumas, por lo que se dividirá entre 4
+    resultado=suma_unitaria(resultado)
+"""
+Termino el código con la función que me va a ayudar a sacar el rango para poder
+clasificar en que precio se van a ir acomodando el diferente tipo de jugadores,
+esto va a estar delimitado por condicionales que van a determinar si el resultado
+de la función "rango" es correspondiente a ciertos precios.
+"""
+
 def rango (n1):
     res_final=n1/4
     return res_final
 var_final= rango(resultado)
-print(var_final)    
+print(var_final)  
 
 """
-Termino el código con la función que me va a ayudar a sacar el rango para poder clasificar en que precio se van a ir acomodando el diferente tipo de jugadores, esto va a estar delimitado
-por condicionales que van a determinar si el resultado de la función "rango" es correspondiente a ciertos precios.
-"""
-"""
-Después de la linea de código de "posición_jugador" se tiene planeado hacer preguntas con variable de tipo boolean, las cuales aun no han sido vistas en clase y por consecuencia no son requerimiento en esta entrega de avance
+Después de la linea de código de "posición_jugador" se tiene planeado hacer
+preguntas con variable de tipo boolean, las cuales aun no han sido vistas en
+clase y por consecuencia no son requerimiento en esta entrega de avance
 del proyecto. En pseudocódigo sería algo mas o menos así:
 
 E0=
@@ -90,7 +118,8 @@ E0=
 seleccion_jug=(bool(input(El jugador ha sido llamado a su seleccion nacional?)))
 si seleccion_jug=True
    resultado=resultado+2
-   seleccion_apariciones=(bool(input(Las veces que fue convocado se desempeñó destacadamente?)))
+   seleccion_apariciones=(bool(input(Las veces que fue convocado se desempeñó
+   destacadamente?)))
       si seleccion_apariciones=True:
          resultado=resultado+3
       else
@@ -99,6 +128,16 @@ else
    resultado=-2
 EF= resultado con valor actualizado
 """
+
+
+
+
+
+    
+     
+   
+
+
    
 
 
